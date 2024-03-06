9e9b50a14de127a62be0b179dd7661351d8d55f9
Vaibhav Shukla <vaibhavshukla264@gmail.com>
2024-03-06 15:20:00 +0530

    changes

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ wss.on('connection', function connection(ws) {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
